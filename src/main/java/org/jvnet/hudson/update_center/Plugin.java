e076346d30b18e47aa253096f2c7304c6695e2fe
alanharder <mindless@dev.java.net>
2011-01-25 09:36:11 -0800

    Some managed a release with github.com:hudson instead of github.com/hudson in the scm/connection.. so accept this in the regexp.

--- src/main/java/org/jvnet/hudson/update_center/Plugin.java
+++ src/main/java/org/jvnet/hudson/update_center/Plugin.java
@@ -157,7 +157,7 @@ public class Plugin {
         return null;
     }
 
-    private static final Pattern HOSTNAME_PATTERN = Pattern.compile("://(?:\\w*@)?([\\w.]+)/");
+    private static final Pattern HOSTNAME_PATTERN = Pattern.compile("://(?:\\w*@)?([\\w.]+)[/:]");
 
     /**
      * Get hostname of SCM specified in POM of latest release, or null.
