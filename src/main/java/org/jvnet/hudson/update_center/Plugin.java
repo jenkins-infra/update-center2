963dd60e853d4873022e10bb84da1194ce1db563
mc1arke <michael.m.clarke@gmail.com>
2013-03-09 08:14:29 +0000

    Javadoc fix

--- src/main/java/org/jvnet/hudson/update_center/Plugin.java
+++ src/main/java/org/jvnet/hudson/update_center/Plugin.java
@@ -65,7 +65,7 @@ public class Plugin {
      */
     public final HPI latest;
     /**
-     * Latest version of this plugin.
+     * Previous version of this plugin.
      */
     public final HPI previous;
     /**
