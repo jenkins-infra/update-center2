ba656c123a721f13cb61f714470efd682dafd2c8
Daniel Beck <daniel-beck@users.noreply.github.com>
2018-07-17 00:51:54 +0200

    OkHttp3 cache

--- src/main/java/org/jvnet/hudson/update_center/GitHubSource.java
+++ src/main/java/org/jvnet/hudson/update_center/GitHubSource.java
@@ -1,14 +1,15 @@
 package org.jvnet.hudson.update_center;
 
-import com.squareup.okhttp.Cache;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.OkUrlFactory;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.OkUrlFactory;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.lang.StringUtils;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
+import org.kohsuke.github.extras.OkHttp3Connector;
 import org.kohsuke.github.extras.OkHttpConnector;
 
 import java.io.File;
@@ -31,8 +32,8 @@ public class GitHubSource {
         try {
             if (GITHUB_API_USERNAME != null && GITHUB_API_PASSWORD != null) {
 
-                Cache cache = new Cache(GITHUB_API_CACHE, 1024L*1024*1024);
-                github = new GitHubBuilder().withConnector(new OkHttpConnector(new OkUrlFactory(new OkHttpClient().setCache(cache)))).withPassword(GITHUB_API_USERNAME, GITHUB_API_PASSWORD).build();
+                Cache cache = new Cache(GITHUB_API_CACHE, 20L*1024*1024); // 20 MB cache
+                github = new GitHubBuilder().withConnector(new OkHttp3Connector(new OkUrlFactory(new OkHttpClient.Builder().cache(cache).build()))).withPassword(GITHUB_API_USERNAME, GITHUB_API_PASSWORD).build();
 
                 this.repoNames = new TreeSet<>(new Comparator<String>() {
                     @Override
