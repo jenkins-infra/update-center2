7249358bbfe473509fc60e5c45b937c93e79e8ae
Daniel Beck <daniel-beck@users.noreply.github.com>
2018-07-20 00:20:23 +0200

    Add "test" for htaccess

---
+++ site/test/redirects.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Simple script to test actual output is sane
+# Ideally we'd set up a local test server with the output, but for now, this will help us quickly figure out if things broke.
+
+# TODO automatically determine these values from current update sites
+CURRENT_WEEKLY=2.133
+RECENT_WEEKLY=2.125
+NEWEST_LTS_BASELINE=2.121
+OLDEST_SUPPORTED_LTS_BASELINE=2.60
+
+set -o pipefail
+set -o errexit
+set -o nounset
+
+function test_redirect () {
+  REQUEST_URL="$1"
+  DESTINATION="$2"
+  echo "Requesting $REQUEST_URL"
+  REDIRECT=$( curl -Ii "$REQUEST_URL" 2>/dev/null | fgrep 'Location:' | cut -d' ' -f2 | tr -d '[:space:]' )
+  if [ $REDIRECT != $DESTINATION ] ; then
+    echo "Expected $DESTINATION but got $REDIRECT for $REQUEST_URL"
+    exit 1
+  fi
+}
+
+test_redirect "https://updates.jenkins.io/update-center.json" "https://updates.jenkins.io/current/update-center.json"
+test_redirect "https://updates.jenkins.io/update-center.json?version=$CURRENT_WEEKLY" "https://updates.jenkins.io/current/update-center.json"
+test_redirect "https://updates.jenkins.io/update-center.json?version=$RECENT_WEEKLY" "https://updates.jenkins.io/current/update-center.json"
+test_redirect "https://updates.jenkins.io/update-center.json?version=$NEWEST_LTS_BASELINE" "https://updates.jenkins.io/$NEWEST_LTS_BASELINE/update-center.json"
+test_redirect "https://updates.jenkins.io/update-center.json?version=$NEWEST_LTS_BASELINE.1" "https://updates.jenkins.io/stable-$NEWEST_LTS_BASELINE/update-center.json"
+test_redirect "https://updates.jenkins.io/update-center.json?version=2.55" "https://updates.jenkins.io/$OLDEST_SUPPORTED_LTS_BASELINE/update-center.json"
+test_redirect "https://updates.jenkins.io/update-center.json?version=2.6" "https://updates.jenkins.io/$OLDEST_SUPPORTED_LTS_BASELINE/update-center.json"
+
+#test_redirect "https://updates.jenkins.io/update-center.json?version=1.650" "https://updates.jenkins.io/$OLDEST_SUPPORTED_LTS_BASELINE/update-center.json"
+#test_redirect "https://updates.jenkins.io/update-center.json?version=1.580" "https://updates.jenkins.io/$OLDEST_SUPPORTED_LTS_BASELINE/update-center.json"
+test_redirect "https://updates.jenkins.io/update-center.json?version=1.580.1" "https://updates.jenkins.io/stable-$OLDEST_SUPPORTED_LTS_BASELINE/update-center.json"
+test_redirect "https://updates.jenkins.io/update-center.json?version=2.46.1" "https://updates.jenkins.io/stable-$OLDEST_SUPPORTED_LTS_BASELINE/update-center.json"
+
+
+# This is probably better -- drop down if older than newest LTS baseline, this isn't getting updates weekly
+#test_redirect "https://updates.jenkins.io/update-center.json?version=2.120" "https://updates.jenkins.io/2.107/update-center.json"
+# This is the current result
+test_redirect "https://updates.jenkins.io/update-center.json?version=2.115" "https://updates.jenkins.io/2.121/update-center.json"
