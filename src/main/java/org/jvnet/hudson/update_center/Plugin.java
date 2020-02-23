d1efb8479fbeca44526b25c64cfdbf18b8ddc1f7
Daniel Beck <daniel-beck@users.noreply.github.com>
2020-02-23 05:19:16 +0100

    Improve handling of the labels whitelist
    Allow non-whitelisted labels from label-definitions.properties

--- src/main/java/org/jvnet/hudson/update_center/Plugin.java
+++ src/main/java/org/jvnet/hudson/update_center/Plugin.java
@@ -48,11 +48,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -406,37 +407,38 @@ public class Plugin {
     public List<String> getLabels() throws IOException {
         String scm = getScmUrl();
 
-        List<String> labels = Arrays.asList(getLabelsFromFile());
-
+        List<String> gitHubLabels = new ArrayList<>();
         if (scm != null && scm.contains("https://github.com/")) {
+
+            List<String> unsanitizedLabels = new ArrayList<>();
             String[] parts = scm.replaceFirst("https://github.com/", "").split("/");
             if (parts.length >= 2) {
-                labels.addAll(
+                unsanitizedLabels.addAll(
                         Arrays.asList(
                                 GitHubSource.getInstance().getRepositoryTopics(parts[0], parts[1]).toArray(new String[0])
                         )
                 );
             }
-        }
 
-        if (!labels.isEmpty()) {
-            HashSet<String> allowedLabels = new HashSet<>();
-
-            for (String label : labels) {
+            for (String label : unsanitizedLabels) {
                 if (label.startsWith("jenkins-")) {
                     label = label.replaceFirst("jenkins-", "");
                 }
 
-                if (ALLOWED_LABELS.containsKey(label)) {
-                    allowedLabels.add(label);
-                } else {
-                    System.err.println(artifactId + " has a label of " + label + " which is not in ALLOWED_LABELS, so dropping");
+                if (ALLOWED_GITHUB_LABELS.containsKey(label)) {
+                    gitHubLabels.add(label);
                 }
             }
 
-            labels = new ArrayList<>(allowedLabels);
+            if (!gitHubLabels.isEmpty()) {
+                System.out.println(artifactId + " got the following labels contributed from GitHub: " + StringUtils.join(gitHubLabels, ", "));
+            }
         }
-        return labels;
+
+        Set<String> labels = new TreeSet<>(Arrays.asList(getLabelsFromFile()));
+        labels.addAll(gitHubLabels);
+
+        return new ArrayList<>(labels);
     }
 
     private String[] getLabelsFromFile() {
@@ -576,13 +578,13 @@ public class Plugin {
 
     private static final Properties URL_OVERRIDES = new Properties();
     private static final Properties LABEL_DEFINITIONS = new Properties();
-    private static final Properties ALLOWED_LABELS = new Properties();
+    private static final Properties ALLOWED_GITHUB_LABELS = new Properties();
 
     static {
         try {
             URL_OVERRIDES.load(Plugin.class.getClassLoader().getResourceAsStream("wiki-overrides.properties"));
             LABEL_DEFINITIONS.load(Plugin.class.getClassLoader().getResourceAsStream("label-definitions.properties"));
-            ALLOWED_LABELS.load(Plugin.class.getClassLoader().getResourceAsStream("allowed-labels.properties"));
+            ALLOWED_GITHUB_LABELS.load(Plugin.class.getClassLoader().getResourceAsStream("allowed-github-topics.properties"));
         } catch (IOException e) {
             throw new Error(e);
         }
