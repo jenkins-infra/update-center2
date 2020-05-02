eb52efb54aa63d08013ea4c6e6ab6a2b6abd7eac
Daniel Beck <daniel-beck@users.noreply.github.com>
2020-05-02 02:32:24 +0200

    New build

--- site/generate.sh
+++ site/generate.sh
@@ -50,7 +50,7 @@ mkdir -p "$WWW_ROOT_DIR"
 
 rm -rfv generator/
 rm -rfv generator.zip
-wget --no-verbose -O generator.zip "https://repo.jenkins-ci.org/snapshots/org/jenkins-ci/update-center2/3.0-SNAPSHOT/update-center2-3.0-20200502.001422-22-bin.zip"
+wget --no-verbose -O generator.zip "https://repo.jenkins-ci.org/snapshots/org/jenkins-ci/update-center2/3.0-SNAPSHOT/update-center2-3.0-20200502.003051-24-bin.zip"
 unzip generator.zip -d generator/
 
 
