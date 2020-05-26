4639abf5930ab077d01bc7771050d6e3287adc70
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2020-05-26 21:02:52 +0200

    [JENKINS-59136] Deprecation support (#344)
    Co-authored-by: Daniel Beck <daniel-beck@users.noreply.github.com>

---
+++ src/main/java/io/jenkins/update_center/Deprecations.java
@@ -0,0 +1,30 @@
+package io.jenkins.update_center;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+public class Deprecations {
+    private Deprecations() {}
+
+    public static String getCustomDeprecationUri(String pluginName) {
+        return DEPRECATIONS.getProperty(pluginName);
+    }
+
+    public static List<String> getDeprecatedPlugins() {
+        return DEPRECATIONS.keySet().stream().map(Object::toString).collect(Collectors.toList());
+    }
+
+    private static final Properties DEPRECATIONS = new Properties();
+
+    static {
+        try {
+            DEPRECATIONS.load(Files.newInputStream(new File(Main.resourcesDir, "deprecations.properties").toPath()));
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+    }
+}
