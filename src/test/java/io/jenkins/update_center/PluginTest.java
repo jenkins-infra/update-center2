3e6247273be0ceb289823359a52ae7e14f8c640d
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2021-03-01 10:45:22 +0100

    Keep older ambiguous releases (#468)
    * Remove ambiguously versioned releases
    
    * Handle more than two duplicates
    
    * In case of ambiguous releases, keep the existing release
    
    Co-authored-by: Daniel Beck <daniel-beck@users.noreply.github.com>

--- src/test/java/io/jenkins/update_center/PluginTest.java
+++ src/test/java/io/jenkins/update_center/PluginTest.java
@@ -1,6 +1,13 @@
 package io.jenkins.update_center;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 public class PluginTest extends TestCase {
 
@@ -49,4 +56,88 @@ public class PluginTest extends TestCase {
         assertNull(HPI.requireTopLevelUrl("https://github.com/jenkinsci/repo/subfolder"));
     }
 
+    @Test
+    public void testDuplicateDetection() throws Exception {
+        Plugin plugin = new Plugin("foo");
+        final RecordingHandler handler = new RecordingHandler();
+        Logger.getLogger(Plugin.class.getName()).addHandler(handler);
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0", "hpi", ""), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0.0", "hpi", ""), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0.0.0", "hpi", ""), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-other-group", "foo", "1.0", "hpi", ""), plugin));
+        assertMessageSubstringLogged(handler, "Found a duplicate artifact the-group:foo:1.0.0 (proposed) considered identical to the-group:foo:1.0 (existing) due to non-determinism. Neither has a timestamp. Neither will be published.");
+        assertMessageSubstringLogged(handler, "Found another duplicate artifact the-group:foo:1.0.0.0 considered identical due to non-determinism. Neither has a timestamp. Neither will be published.");
+        assertMessageSubstringLogged(handler, "Found another duplicate artifact the-other-group:foo:1.0 considered identical due to non-determinism. Neither has a timestamp. Neither will be published.");
+        assertTrue("No versions", plugin.getArtifacts().isEmpty());
+    }
+
+    public void testKeepOlder() throws Exception {
+        Plugin plugin = new Plugin("foo");
+        final RecordingHandler handler = new RecordingHandler();
+        Logger.getLogger(Plugin.class.getName()).addHandler(handler);
+        final HPI first = new HPI(null, new ArtifactCoordinates("the-group", "foo", "1.0", "hpi", "", 1), plugin);
+        plugin.addArtifact(first);
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0.0", "hpi", "", 2), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0.0.0", "hpi", "",1), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-other-group", "foo", "1.0", "hpi", "", 4), plugin));
+        assertMessageSubstringLogged(handler, "The proposed artifact: the-group:foo:1.0.0 is not older than the existing artifact the-group:foo:1.0, so ignore it.");
+        assertMessageSubstringLogged(handler, "The proposed artifact: the-group:foo:1.0.0.0 is not older than the existing artifact the-group:foo:1.0, so ignore it.");
+        assertMessageSubstringLogged(handler, "The proposed artifact: the-other-group:foo:1.0 is not older than the existing artifact the-group:foo:1.0, so ignore it.");
+        assertEquals("One artifact", 1, plugin.getArtifacts().size());
+        assertEquals("Original artifact retained", plugin.getArtifacts().firstEntry().getValue(), first);
+    }
+
+    public void testReplaceAll() throws Exception {
+        Plugin plugin = new Plugin("foo");
+        final RecordingHandler handler = new RecordingHandler();
+        Logger.getLogger(Plugin.class.getName()).addHandler(handler);
+        plugin.addArtifact(new HPI(null, new ArtifactCoordinates("the-group", "foo", "1.0", "hpi", "", 4), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0.0", "hpi", "", 3), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0.0.0", "hpi", "",2), plugin));
+        final HPI oldest = new HPI(null, new ArtifactCoordinates("the-other-group", "foo", "1.0", "hpi", "", 1), plugin);
+        plugin.addArtifact(oldest);
+        assertMessageSubstringLogged(handler, "The proposed artifact: the-group:foo:1.0.0 is older than the existing artifact the-group:foo:1.0, so replace it.");
+        assertMessageSubstringLogged(handler, "The proposed artifact: the-group:foo:1.0.0.0 is older than the existing artifact the-group:foo:1.0.0, so replace it.");
+        assertMessageSubstringLogged(handler, "The proposed artifact: the-other-group:foo:1.0 is older than the existing artifact the-group:foo:1.0.0.0, so replace it.");
+        assertEquals("One artifact", 1, plugin.getArtifacts().size());
+        assertEquals("Original artifact retained", plugin.getArtifacts().firstEntry().getValue(), oldest);
+    }
+
+    public void testRemoveAll() throws Exception {
+        Plugin plugin = new Plugin("foo");
+        final RecordingHandler handler = new RecordingHandler();
+        Logger.getLogger(Plugin.class.getName()).addHandler(handler);
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0", "hpi", "", 0), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0.0", "hpi", "", 42), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-group", "foo", "1.0.0.0", "hpi", "", 42), plugin));
+        plugin.addArtifact(new HPI(null , new ArtifactCoordinates("the-other-group", "foo", "1.0", "hpi", "", 0), plugin));
+        assertMessageSubstringLogged(handler, "The proposed artifact: the-group:foo:1.0.0 has a timestamp and the existing artifact the-group:foo:1.0 does not, so replace it.");
+        assertMessageSubstringLogged(handler, "The proposed artifact: the-group:foo:1.0.0.0 is not older than the existing artifact the-group:foo:1.0.0, so ignore it.");
+        assertMessageSubstringLogged(handler, "The proposed artifact: the-other-group:foo:1.0 has no timestamp (but the existing artifact the-other-group:foo:1.0 does), so ignore it.");
+        assertEquals("One artifact", 1, plugin.getArtifacts().size());
+    }
+
+    private static void assertMessageSubstringLogged(RecordingHandler handler, String message) {
+        assertTrue("Message logged: " + message, handler.records.stream().anyMatch(it -> it.getMessage().contains(message)));
+    }
+
+    private static class RecordingHandler extends Handler {
+
+        private List<LogRecord> records = new ArrayList<>();
+
+        @Override
+        public void publish(LogRecord record) {
+            records.add(record);
+        }
+
+        @Override
+        public void flush() {
+
+        }
+
+        @Override
+        public void close() throws SecurityException {
+
+        }
+    }
 }
