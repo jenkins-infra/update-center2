30914077281922e58b873deea5fd45dd43fad4d5
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2022-06-27 13:20:03 +0200

    Split cache into sha prefix based directories (#609)
    Co-authored-by: Daniel Beck <daniel-beck@users.noreply.github.com>

--- src/main/java/io/jenkins/update_center/ArtifactoryRepositoryImpl.java
+++ src/main/java/io/jenkins/update_center/ArtifactoryRepositoryImpl.java
@@ -228,11 +228,18 @@ public class ArtifactoryRepositoryImpl extends BaseMavenRepository {
     }
 
     private File getFile(final String url) throws IOException {
+        // TODO remove old base64 based cache paths once the cache is migrated
         String urlBase64 = Base64.encodeBase64String(new URL(url).getPath().getBytes(StandardCharsets.UTF_8));
         File cacheFile = new File(cacheDirectory, urlBase64);
         if (!cacheFile.exists()) {
             // Preferred new location (guaranteed maximum filename length):
-            cacheFile = new File(cacheDirectory, DigestUtils.sha256Hex(url));
+            final String sha256 = DigestUtils.sha256Hex(url);
+            final String sha256prefix = sha256.substring(0, 2); // to limit number of files in top-level directory
+            final File cachePrefixDir = new File(cacheDirectory, sha256prefix);
+            if (!cachePrefixDir.exists() && !cachePrefixDir.mkdirs()) {
+                LOGGER.log(Level.WARNING, "Failed to create cache prefix directory " + cachePrefixDir);
+            }
+            cacheFile = new File(cachePrefixDir, sha256);
         }
 
         if (!cacheFile.exists()) {
