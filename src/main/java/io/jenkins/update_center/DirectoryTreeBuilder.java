9a986d4c392351dee171ab078c802b6d4b1e9b32
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2022-08-11 22:01:42 +0200

    Suppress FB warning about ProcessBuilder invoking ln (#634)
    Co-authored-by: Daniel Beck <daniel-beck@users.noreply.github.com>

--- src/main/java/io/jenkins/update_center/DirectoryTreeBuilder.java
+++ src/main/java/io/jenkins/update_center/DirectoryTreeBuilder.java
@@ -1,5 +1,6 @@
 package io.jenkins.update_center;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.util.VersionNumber;
 import org.apache.commons.io.IOUtils;
 import org.kohsuke.args4j.Option;
@@ -144,6 +145,8 @@ public class DirectoryTreeBuilder {
      * @param dst the staging location
      * @throws IOException when a problem occurs during file operations
      */
+    @SuppressFBWarnings(value="COMMAND_INJECTION",
+            justification="No injection risk from absolute path args to ln -s")
     protected void stage(MavenArtifact a, File dst) throws IOException {
         File src = a.resolve();
         if (dst.exists() && dst.lastModified() == src.lastModified() && dst.length() == src.length()) {
