cf1e8faa14ffe80a5c6b145773cb5b2321011930
Gavin Mogan <github@gavinmogan.com>
2023-01-05 03:00:17 -0800

    Update site/generate-jenkins-io-template.sh
    Co-authored-by: Daniel Beck <1831569+daniel-beck@users.noreply.github.com>

--- site/generate-jenkins-io-template.sh
+++ site/generate-jenkins-io-template.sh
@@ -3,6 +3,10 @@
 WWW_ROOT_DIR="${WWW_ROOT_DIR:-${1:-./www2}}"
 SELF_URL="${SELF_URL:-${2:-https://updates.jenkins.io}}"
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 wget --quiet --convert-links --output-document "$WWW_ROOT_DIR/index.html" --convert-links https://www.jenkins.io/templates/updates/index.html
 sed -i.bak -e "s|<jio-navbar.*/jio-navbar>|<jio-navbar class='fixed-top' id='ji-toolbar' property='${SELF_URL}'></jio-navbar>|g" "$WWW_ROOT_DIR/index.html"
 sed -i.bak -e "s|<jio-footer.*/jio-footer>|<jio-footer property='${SELF_URL}'></jio-footer>|g" "$WWW_ROOT_DIR/index.html"
