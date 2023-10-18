3aca271082ba9dfde4dc03f03abcd546702eacde
smerle33 <stephane.merle+jenkins@gmail.com>
2023-10-18 14:50:13 +0200

    parrallilization

--- site/publish.sh
+++ site/publish.sh
@@ -9,6 +9,9 @@ if [[ -z "$ROOT_FOLDER" ]]; then
     ROOT_FOLDER="/home/jenkins/lemeurherve/pr-745" # TODO: remove after debug
 fi
 
+# parallel added within the permanent trusted agent here : https://github.com/jenkins-infra/jenkins-infra/blob/production/dist/profile/manifests/buildagent.pp
+command -v parallel >/dev/null 2>&1 || { echo "ERROR: parralel command not found. Exiting."; exit 1; }
+
 echo "ROOT_FOLDER: ${ROOT_FOLDER}"
 
 wget --no-verbose -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 || { echo "Failed to download jq" >&2 ; exit 1; }
@@ -34,35 +37,65 @@ chmod -R a+r "${ROOT_FOLDER}"/www2
 
 # ### TODO: cleanup original commands above when https://github.com/jenkins-infra/helpdesk/issues/2649 is ready for production
 
-# Original-like rsync to pkg VM for testing and timing purposes
-time rsync -acvz "${ROOT_FOLDER}"/www2/ --exclude=/updates --delete --stats ${RSYNC_USER}@${UPDATES_SITE}:/tmp/lemeurherve/pr-745/www/${UPDATES_SITE}
-
-### Above ^: not to be modified
-
-### Below: parallelise
-
+####   no need to remove the symlinks as the `azcopy sync`for symlinks is not yet supported and we use `--no-follow-symlinks` for `aws s3 sync``
+# Perform a copy with dereference symlink (object storage do not support symlinks)
 # copy & transform simlinks into referent file/dir
-# time rsync -acvz --copy-links --safe-links --stats "${ROOT_FOLDER}"/www2/ --exclude=/updates --delete "${ROOT_FOLDER}"/www3/
-time rsync -acvz --no-links --stats "${ROOT_FOLDER}"/www2/ --exclude=/updates --delete "${ROOT_FOLDER}"/www3/
+time rsync  -acvz \
+            --copy-links `# derefence symlinks` \
+            --safe-links `# ignore symlinks outside of copied tree` \
+            --stats `# add verbose statistics` \
+            "${ROOT_FOLDER}"/www2/ --exclude=updates/ --delete "${ROOT_FOLDER}"/www3/
+## "${ROOT_FOLDER}"/www3/ doesn't have symlinks already
+## "${ROOT_FOLDER}"/www2/ still have symlinks
+### Below: parallelise
+echo '--------------------------- Launch Parallelization -----------------------'
 
-ls -l "${ROOT_FOLDER}"/www3/
-ls -l "${ROOT_FOLDER}"/www3/current
 
-# Sync Azure File Share content
-time azcopy sync "${ROOT_FOLDER}"/www3/ "${UPDATES_FILE_SHARE_URL}" --recursive=true --delete-destination=true --exclude-path="updates"
+## define function
+function parallelfunction() {
+    echo "=== parallelfunction: $1"
 
-# Debug
-echo "= azcopy sync done."
+    case $1 in
+    rsync*)
+        # keep exclude as from www2 with symlinks
+        time rsync -acz "${ROOT_FOLDER}"/www2/ --exclude=/updates --delete --stats ${RSYNC_USER}@${UPDATES_SITE}:/tmp/lemeurherve/pr-745/www/${UPDATES_SITE}
+        ;;
 
-# Sync CloudFlare R2 buckets content using the updates-jenkins-io profile, excluding 'updates' folder which comes from tool installer generator
-time aws s3 sync "${ROOT_FOLDER}"/www3/ s3://"${UPDATES_R2_BUCKETS}"/ --profile updates-jenkins-io --no-progress --size-only --no-follow-symlinks --exclude="updates/*" --endpoint-url "${UPDATES_R2_ENDPOINT}"
-# aws s3 cp "${ROOT_FOLDER}"/www2/ s3://"${UPDATES_R2_BUCKETS}"/ --profile updates-jenkins-io --no-progress --no-follow-symlinks --exclude="updates/*" --endpoint-url "${UPDATES_R2_ENDPOINT}"
+    azsync*)
+        # Sync Azure File Share content (using www3 to avoid symlinks)
+        time azcopy sync "${ROOT_FOLDER}"/www3/ "${UPDATES_FILE_SHARE_URL}" --recursive=true --delete-destination=true
+        ;;
 
+    s3sync*)
+        # Sync CloudFlare R2 buckets content using the updates-jenkins-io profile, excluding 'updates' folder which comes from tool installer generator (using www3 to avoid symlinks)
+        time aws s3 sync "${ROOT_FOLDER}"/www3/ s3://"${UPDATES_R2_BUCKETS}"/ --profile updates-jenkins-io --no-progress --no-follow-symlinks --size-only --exclude '.htaccess' --endpoint-url "${UPDATES_R2_ENDPOINT}"
+        ;;
+
+    *)
+        echo -n "unknown"
+        ;;
+    esac
+
+}
+
+## need to export variables used within the functions above
+export UPDATES_SITE
+export RSYNC_USER
+export UPDATES_R2_BUCKETS
+export UPDATES_R2_ENDPOINT
+export ROOT_FOLDER
+
+## export function to use with parallel
+export -f parallelfunction
+parallel --halt-on-error now,fail=1 parallelfunction ::: rsync azsync s3sync
+
+
+# wait for all deferred task
+echo '===============================    all done   ============================'
 
 ## TODO: test if needed rclone both rsync VM and R2 bucket(s) replacing these 2 calls
 
 # Debug
-echo "= aws sync done."
 
 # # /TIME sync, used by mirrorbits to know the last update date to take in account
 # date +%s > ./www2/TIME
