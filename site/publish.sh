70c1d91f994983bfb012a464fd0f030ced066877
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2023-11-17 15:38:27 +0100

    array of R2 buckets

--- site/publish.sh
+++ site/publish.sh
@@ -7,19 +7,9 @@
 UPDATES_SITE="updates.jenkins.io"
 RSYNC_USER="www-data"
 
-# For syncing R2 buckets with aws-cli configured through environment variables (from Jenkins credentials)
+# For syncing R2 buckets aws-cli is configured through environment variables (from Jenkins credentials)
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
 export AWS_DEFAULT_REGION="auto"
-UPDATES_R2_BUCKET="westeurope-updates-jenkins-io"
-UPDATES_R2_ENDPOINT="https://8d1838a43923148c5cee18ccc356a594.r2.cloudflarestorage.com"
-
-# For syncing Azure File Share
-UPDATES_FILE_SHARE_URL_AND_PATH="https://updatesjenkinsio.file.core.windows.net/updates-jenkins-io/"
-
-# For triggering a mirror scan on mirrorbits
-MIRRORBITS_POD_NAME_PREFIX="mirrorbits-lite"
-MIRRORBITS_CONTAINER_NAME="mirrorbits-lite"
-MIRRORBITS_NAMESPACE="updates-jenkins-io"
 
 ## Install jq, required by generate.sh script
 wget --no-verbose -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 || { echo "Failed to download jq" >&2 ; exit 1; }
@@ -71,20 +61,25 @@ function parallelfunction() {
 
     azsync*)
         # Sync Azure File Share content using www3 to avoid symlinks
-        time azcopy sync ./www3/ "${UPDATES_FILE_SHARE_URL_AND_PATH}?${UPDATES_FILE_SHARE_QUERY_STRING}" \
+        time azcopy sync ./www3/ "https://updatesjenkinsio.file.core.windows.net/updates-jenkins-io/?${UPDATES_FILE_SHARE_QUERY_STRING}" \
             --recursive=true \
             --delete-destination=true
         ;;
 
     s3sync*)
+        # Retrieve the R2 bucket and the R2 endpoint from the task name passed as argument, minus "s3sync" prefix
+        updates_r2_bucket_and_endpoint="${1#s3sync}"
+        r2_bucket=${updates_r2_bucket_and_endpoint%|*}
+        r2_endpoint=${updates_r2_bucket_and_endpoint#*|}
+
         # Sync CloudFlare R2 buckets content excluding 'updates' folder from www3 sync (without symlinks)
         # as this folder is populated by https://github.com/jenkins-infra/crawler/blob/master/Jenkinsfile
-        time aws s3 sync ./www3/ s3://"${UPDATES_R2_BUCKET}"/ \
+        time aws s3 sync ./www3/ s3://"${r2_bucket}"/ \
             --no-progress \
             --no-follow-symlinks \
             --size-only \
             --exclude '.htaccess' \
-            --endpoint-url "${UPDATES_R2_ENDPOINT}"
+            --endpoint-url "${r2_endpoint}"
         ;;
 
     *)
@@ -97,35 +92,37 @@ function parallelfunction() {
 # Export local variables used in parallelfunction
 export UPDATES_SITE
 export RSYNC_USER
-export UPDATES_R2_BUCKET
-export UPDATES_R2_ENDPOINT
-export UPDATES_FILE_SHARE_URL_AND_PATH
 
 # Export function to use it with parallel
 export -f parallelfunction
 
-echo '----------------------- Launch synchronisation(s) -----------------------'
-if [[ $OPT_IN_SYNC_FS_R2 == 'optin' ]]
-then
-    # Sync updates.jenkins.io and azure.updates.jenkins.io
-    parallel --halt-on-error now,fail=1 parallelfunction ::: rsync azsync s3sync
-else
-    # Sync only updates.jenkins.io
-    parallel --halt-on-error now,fail=1 parallelfunction ::: rsync
+# Sync only updates.jenkins.io by default
+tasks=("rsync")
 
-    # ## If we prefer to avoid parallel when not opt-in, we can replace the previous instruction by the following one:
-    # # push generated index to the production server
-    # rsync --archive --checksum --verbose --compress \
-    #     --exclude=/updates `# populated by https://github.com/jenkins-infra/crawler` \
-    #     --delete `# delete old sites` \
-    #     --stats `# add verbose statistics` \
-    #     ./www2/ ${RSYNC_USER}@${UPDATES_SITE}:/var/www/${UPDATES_SITE}
+# Sync updates.jenkins.io and azure.updates.jenkins.io File Share and R2 bucket(s) if the flag is set
+if [[ $OPT_IN_SYNC_FS_R2 == "optin" ]]
+then
+    # Add File Share sync to the tasks
+    tasks+=("azsync")
+
+    # Add each R2 bucket sync to the tasks
+    updates_r2_bucket_and_endpoint_pairs=("westeurope-updates-jenkins-io|https://8d1838a43923148c5cee18ccc356a594.r2.cloudflarestorage.com")
+    for r2_bucket_and_endpoint_pair in "${updates_r2_bucket_and_endpoint_pairs[@]}"
+    do
+        tasks+=("s3sync${r2_bucket_and_endpoint_pair}")
+    done
 fi
+
+echo '----------------------- Launch synchronisation(s) -----------------------'
+parallel --halt-on-error now,fail=1 parallelfunction ::: "${tasks[@]}"
 
 # Wait for all deferred tasks
 echo '============================ all done ============================'
 
 echo '== Triggering a mirror scan on mirrorbits...'
+# Kubernetes namespace of mirrorbits
+mirrorbits_namespace="updates-jenkins-io"
+
 # Requires a valid kubernetes credential file at $KUBECONFIG or $HOME/.kube/config by default
-pod_name="$(kubectl --namespace=${MIRRORBITS_NAMESPACE} --no-headers=true get pod --output=name | grep "${MIRRORBITS_POD_NAME_PREFIX}" | head -n1)"
-kubectl --namespace=${MIRRORBITS_NAMESPACE} --container="${MIRRORBITS_CONTAINER_NAME}" exec "${pod_name}" -- mirrorbits scan -all -enable -timeout=120
+pod_name="$(kubectl --namespace=${mirrorbits_namespace} --no-headers=true get pod --output=name | grep mirrorbits-lite | head -n1)"
+kubectl --namespace=${mirrorbits_namespace} --container=mirrorbits-lite exec "${pod_name}" -- mirrorbits scan -all -enable -timeout=120
