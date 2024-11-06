11fb4848b1a1eb854e4188a251f08cbf95c25f48
Damien Duportal <damien.duportal@gmail.com>
2024-11-06 15:39:50 +0100

    Apply suggestions from code review

--- site/publish.sh
+++ site/publish.sh
@@ -160,7 +160,7 @@ then
         "${www2_dir}"/ "${content_dir}"/
 
     # create an empty folder for the cawler content to avoid error with deference symlinks
-    mkdir -p "${www2_dir}"/current/updates
+    mkdir -p "${www2_dir}"/updates
     # Prepare www-redirections-*secured/ directories, same content as $www2_dir (to allow directory listing) but with dereferenced symlinks, dedicated to httpd services
     rsync --archive --verbose \
         --copy-links `# derefence symlinks` \
